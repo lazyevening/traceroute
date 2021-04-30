2d47894b06d294e55cb4da269391434d8610f357
Artem Ikonnikov <lazyevening@yandex.ru>
2021-04-30 14:46:06 +0500

    miss handling added

--- tracerouter.py
+++ tracerouter.py
@@ -3,15 +3,24 @@ import subprocess
 import re
 
 
+def decode_line(line):
+    decoded_line = line.decode('CP866')
+    print(decoded_line)
+    match_ip = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", decoded_line)
+    match_miss = re.findall(r"\* {8}\* {8}\*", decoded_line)
+    if match_ip:
+        return match_ip
+    elif match_miss:
+        return "****"
+
+
 def trace(address: str):
     route = []
     data = subprocess.check_output(["tracert", address]).splitlines()
     for line in data:
-        decoded_line = line.decode('CP866')
-        # print(decoded_line)
-        match = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", decoded_line)
-        if match:
-            route.append(match)
+        dec_line = decode_line(line)
+        if dec_line is not None:
+            route.append(dec_line)
     route = route[1:]
     return route
 
